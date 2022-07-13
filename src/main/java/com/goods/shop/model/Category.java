d7a71d937ecdaec937b4afaa313b21110eeda3b7
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-07-13 22:46:35 +0300

    Added checkstyle plugin, fixded models

--- src/main/java/com/goods/shop/model/Category.java
+++ src/main/java/com/goods/shop/model/Category.java
@@ -1,8 +1,17 @@
 package com.goods.shop.model;
 
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- src/main/java/com/goods/shop/model/Product.java
+++ src/main/java/com/goods/shop/model/Product.java
@@ -2,10 +2,23 @@ package com.goods.shop.model;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
     private BigDecimal price;
 
