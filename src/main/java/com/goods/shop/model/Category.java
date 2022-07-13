e66fb5898c4d7f8e026f0e9cadd2894b883387b2
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-07-13 20:18:40 +0300

    Created basic config and models

---
+++ src/main/java/com/goods/shop/model/Category.java
@@ -0,0 +1,51 @@
+package com.goods.shop.model;
+
+import java.util.Objects;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Category category = (Category) object;
+
+        return Objects.equals(id, category.id)
+                && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
