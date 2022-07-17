2e9194882ed5b5a9aad94c78374566f07ef0dc22
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-07-17 19:05:03 +0300

    Added ShoppingCart

---
+++ src/main/java/com/goods/shop/model/ShoppingCart.java
@@ -0,0 +1,78 @@
+package com.goods.shop.model;
+
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "id")
+    private User user;
+    @OneToMany
+    @JoinTable(name = "shopping_carts_products",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setTickets(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        ShoppingCart shoppingCart = (ShoppingCart) object;
+
+        return Objects.equals(id, shoppingCart.id)
+                && Objects.equals(user, shoppingCart.user)
+                && Objects.equals(products, products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, products != null ? products : 0 );
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", products=" + products
+                + ", user=" + user
+                + '}';
+    }
+}
--- src/main/java/com/goods/shop/repository/ShoppingCartRepository.java
+++ src/main/java/com/goods/shop/repository/ShoppingCartRepository.java
@@ -1,13 +1,12 @@
 package com.goods.shop.repository;
 
-import com.goods.shop.model.Order;
+import com.goods.shop.model.ShoppingCart;
 import com.goods.shop.model.User;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findAllByUser(User user);
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    ShoppingCart findByUser(User user);
 
 }
